141e50c5c5751a96ac73333eaabec451199bded4
saltonbench <jlin22@stuy.edu>
2017-12-10 14:48:37 -0500

    started sem and shm stuff on client

--- control.c
+++ control.c
@@ -14,15 +14,14 @@
 
 
 int main(int argc, char * argv[]){
-  int semid;
   if(argc==2){
     if (strcmp(argv[1],"-c")==0){
-      semid =semget(SEMKEY,1, IPC_CREAT | IPC_EXCL | 0600);
+      int semid =semget(SEMKEY,1, IPC_CREAT | IPC_EXCL | 0644);
+      int shmid =shmget(SHMKEY, sizeof(int), IPC_CREAT | IPC_EXCL | 0600);
       if (semid < 0){
 	printf("stuff already exists\n");
       }
       else{
-	int shmid =shmget(SHMKEY, sizeof(int), IPC_CREAT | IPC_EXCL | 0600);
 	semctl(semid, 0, SETVAL, 1);	
 	int fd = open("story.txt", O_CREAT | O_TRUNC, 0644);
 	close(fd);
@@ -30,21 +29,39 @@ int main(int argc, char * argv[]){
       }
     }
     else if (strcmp(argv[1], "-v")==0){
+      
       printf("story : \n");
       execlp("cat","cat","story.txt",NULL);
     }
     else if (strcmp(argv[1],"-r")==0){
-      semid = semget(SEMKEY,1,0600);
+      int semid = semget(SEMKEY,1,0600);
       int r = semctl(semid,0,IPC_RMID);
-      printf("semaphore removed : %d\n", r);
+      if (r < 0){
+	printf("semaphore doesn't exist\n");
+      }
+      else{
+	printf("semaphore removed : %d\n", r);
+      }
       int shmid = shmget(SHMKEY, sizeof(int), 0644);
       int s= shmctl(shmid, IPC_RMID,0);
+      if (s < 0){
+	printf("shared memory doesn't exist\n");
+      }
+      else{
       printf("shared memory removed : %d\n", s);
+      }
       int f = fork();
       int status;
       if (f==0){
-	printf("story : \n");
-	execlp("cat","cat","story.txt",NULL);
+	int fd = open("story.txt", O_RDONLY);
+        if (fd < 0){
+	  printf("story doesn't exist \n");
+	}
+	else{
+	  close("story.txt");
+	  printf("story : \n");
+	  execlp("cat","cat","story.txt",NULL);
+	}
       }
       else{
 	wait(&status);
